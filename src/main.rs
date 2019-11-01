941bec21b1e3c24f0f36f024af84d9aa7fafe9fe
Ryan Joseph <ryan@proxy.com>
2019-11-01 16:25:18 -0700

    Simple tracking

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,33 @@ extern crate regex;
 extern crate redis;
 
 use std::env;
-//use std::thread;
+use std::collections::HashMap;
 use regex::Regex;
 use redis::{PubSubCommands, ControlFlow, IntoConnectionInfo};
 
 const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
 const APP_VER: &'static str = env!("CARGO_PKG_VERSION");
+
+fn monitor_pubsub_channels(host_name: &String, r_client: &redis::Client) -> () {
+  let mut total_count: i64 = 0;
+  let mut channel_tracker: HashMap<String, i64> = HashMap::new();
+  let msg_handler = |msg: redis::Msg| -> redis::ControlFlow<()> {
+    let chan_name = msg.get_channel_name().to_string();
+    let chan_entry = channel_tracker.entry(chan_name.clone()).or_insert(0);
+    total_count += 1;
+    *chan_entry += 1;
+    println!("({}) {}: {}", total_count, &chan_name, *chan_entry);
+    ControlFlow::Continue
+  };
+  
+  print!("Connecting to redis host '{}'...", host_name);
+  let mut conn = r_client.get_connection().expect("redis::Client::get_connection()");
+  println!("success.");
+  match conn.psubscribe(&["*"], msg_handler) {
+    Ok(_) => (),
+    Err(e) => panic!("{}", e)
+  }
+}
 
 fn main() {
   println!("{} v{} starting...", APP_NAME, APP_VER);
@@ -16,7 +37,7 @@ fn main() {
 
   match env::var("REDIS_URL") {
     Ok(r_url) => {
-      let filter_re = Regex::new(r"redis://.*?:.*@(.*)(?:/\d+)").expect("Bad redis URL filter regex");
+      let filter_re = Regex::new(r"redis://(?:(?:.*?:)?.*@)?([^/]+)(?:/\d+)?").expect("Bad redis URL filter regex");
       let caps = filter_re.captures(&r_url).expect("captures() failed");
       if caps.len() < 2 {
         panic!("Badly-formatted redis URL '{}'", r_url);
@@ -31,14 +52,8 @@ fn main() {
 
   match connect_url.into_connection_info() {
     Ok(c_info) => {
-      print!("Connecting to redis host '{}'... ", host_name);
       let r_conn = redis::Client::open(c_info).expect("redis::Client::open()");
-      let mut conn = r_conn.get_connection().expect("redis::Client::get_connection()");
-      println!("success.");
-      conn.psubscribe(&["*"], |msg| -> redis::ControlFlow<()> {
-        println!("MSG in '{}':\n{}\n", msg.get_channel_name(), msg.get_payload::<String>().unwrap());
-        ControlFlow::Continue
-      }).expect("psubscribe failed");
+      monitor_pubsub_channels(&host_name, &r_conn);
     },
     Err(e) => {
       panic!("Bad connection information: {}", e);
