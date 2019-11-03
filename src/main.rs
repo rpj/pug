a3f10812bc886e88429978382385afb2fed0931c
Ryan Joseph <ryan@proxy.com>
2019-11-02 23:58:23 -0700

    Add config, initial watcher impl

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 extern crate config;
 extern crate redis;
 
-use std::env;
-use std::thread;
+use std::{env, thread, time};
 use std::collections::HashMap;
 use config::{Config, File, Environment};
-use redis::{PubSubCommands, ControlFlow};
+use redis::{Commands, PubSubCommands, ControlFlow};
 
 const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
 const APP_VER: &'static str = env!("CARGO_PKG_VERSION");
 
-fn monitor_all(r_client: &redis::Client) -> thread::JoinHandle<()> {
-  print!("monitor_all connecting...");
+const DEFAULT_REDIS_PORT: u16 = 6379;
+const DEFAULT_WAIT_FUDGE_PERCENT: f64 = 0.95;
+
+fn monitor_all(r_client: &redis::Client, verbose: bool) -> thread::JoinHandle<()> {
+  print!("monitor_all verbose={}; connecting...", verbose);
   let mut conn = r_client.get_connection().expect("get_connection()");
   println!("success.");
 
@@ -24,7 +26,8 @@ fn monitor_all(r_client: &redis::Client) -> thread::JoinHandle<()> {
       let chan_entry = channel_tracker.entry(chan_name.clone()).or_insert(0);
       total_count += 1;
       *chan_entry += 1;
-      println!("({}) {}: {}", total_count, &chan_name, *chan_entry);
+      if verbose { println!("monitor_all> ({}) {}: {}", total_count, &chan_name, *chan_entry); }
+      else if total_count % 100 == 0 { println!("monitor_all> {} messages seen", total_count); }
       ControlFlow::Continue
     };
 
@@ -32,14 +35,46 @@ fn monitor_all(r_client: &redis::Client) -> thread::JoinHandle<()> {
   })
 }
 
-fn watch_devices(r_client: &redis::Client, watch: Vec<String>) -> thread::JoinHandle<()> {
-  print!("watch_devices connecting...");
+fn watch_devices(r_client: &redis::Client, watch: Vec<String>, fudge: f64) -> thread::JoinHandle<()> {
+  print!("watch_devices has watch.len={}, fudge={}; connecting...", watch.len(), fudge);
   let mut conn = r_client.get_connection().expect("get_connection()");
   println!("success.");
 
   thread::spawn(move || {
-    if watch.len() > 0 {
-      println!("NEED TO WATCH <{:?}>", watch);
+    let mut all_keys: HashMap<String, i64> = HashMap::new();
+    let mut min_ttl = std::i64::MAX;
+    let mut prev_count = 0;
+
+    for wp in watch {
+      let watch_keys = conn.keys::<String, Vec<String>>(wp.clone()).unwrap_or(vec![]);
+      println!("pattern '{}' has {} to watch", wp, watch_keys.len());
+      for wk in watch_keys {
+        let ttl = conn.ttl::<String, i64>(wk.clone()).expect("ttl");
+        println!("'{}' has ttl {}", wk, ttl);
+        all_keys.entry(wk).or_insert(ttl);
+        min_ttl = if ttl < min_ttl { ttl } else { min_ttl };
+        prev_count += 1;
+      }
+    }
+
+    loop {
+      let sleep_ms: u64 = ((min_ttl as f64 * fudge) * 1000.0) as u64;
+      println!("watch thread sleeping for {}ms", sleep_ms);
+      thread::sleep(time::Duration::from_millis(sleep_ms));
+      let mut count = 0;
+      let new_ttls = all_keys.keys().map(|k| {
+          count += 1;
+          let ttl = conn.ttl::<String, i64>(k.clone()).expect("ttl");
+          //all_keys.insert(k.clone(), ttl);
+          println!("{} ttl -> {}", k, ttl);
+          ttl
+        });
+      min_ttl = new_ttls.min().expect("new min ttl");
+      println!("WATCH THREAD AWAKE new min {}, {} vs {}", min_ttl, prev_count, count);
+      if prev_count < count {
+        println!("!!!!! Lost one!");
+      }
+      prev_count = count;
     }
   })
 }
@@ -59,7 +94,7 @@ fn main() {
   let r_conn_info = redis::ConnectionInfo {
     addr: Box::new(redis::ConnectionAddr::Tcp(
       r_host, 
-      settings.get::<u16>("redis.port").unwrap_or(6379))
+      settings.get::<u16>("redis.port").unwrap_or(DEFAULT_REDIS_PORT))
     ),
     db: 0, 
     passwd: match settings.get_str("redis.password") {
@@ -71,8 +106,11 @@ fn main() {
   let r_conn = redis::Client::open(r_conn_info).expect("redis::Client::open()");
 
   for jh in vec![
-    monitor_all(&r_conn), 
-    watch_devices(&r_conn, settings.get::<Vec<String>>("redis.watch").unwrap_or(vec![]))
+    monitor_all(&r_conn, settings.get_bool("monitor.verbose").unwrap_or(false)), 
+    watch_devices(&r_conn, 
+      settings.get::<Vec<String>>("redis.watch.patterns").unwrap_or(vec![]),
+      settings.get_float("redis.watch.ttl_percentage").unwrap_or(DEFAULT_WAIT_FUDGE_PERCENT)
+    )
   ] {
     jh.join().expect("join");
   }
